c2d7e3e56406385ac5475208086a2c7a40246902
Atharva <74077285+hiowfhowhfhoiwf@users.noreply.github.com>
2023-09-18 19:41:55 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,59 @@
 import cv2 as cv 
 import numpy as np 
 import matplotlib.pyplot as plt 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 from tensorflow.keras import datasets, layers, models
 
+
 (training_images, training_labels),(testing_images, testing_labels) = datasets.cifar10.load_data()
-training_images, testing_images = training_images /255, testing_images/255
+training_images, testing_images = training_images/255, testing_images/255
 
 class_names = ['Plane','Car','Bird','Cat','Deer','Dog','Frog','Horse','Ship','Truck']
+
 
 for i in range(16):
     plt.subplot(4,4,i+1)
     plt.xticks([])
     plt.yticks([])
-    plt.imshow(training_images[i],cmap = plt.cm.binary)
+    plt.imshow(training_images[i], cmap = plt.cm.binary)
     plt.xlabel(class_names[training_labels[i][0]])
 
 plt.show()
+
+#restrict trainign and testing images for computational power
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+#create the model
+model = models.Sequential()
+model.add(layers.Conv2D(32,(3,3),activation='relu',input_shape=(32,32,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3),activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3),activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64,activation='relu'))
+model.add(layers.Dense(10,activation = 'softmax'))
+
+#compile model 
+model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+
+#epochs = how many times u will check the data; epoch = 10, means 10 times check the data 
+model.fit(training_images, training_labels, epochs = 10, validation_data = (testing_images, testing_labels))
+
+loss,accuracy = model.evaluate(testing_images,testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+
+model.save('image_classifier.model')
+
+#to load the model: 
+#model = models.load_model('image_classifier.model')
+
 
 
 
